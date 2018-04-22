ede2b82b23361acf8cdd3cfb8c6c92f38280dc55
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 05:10:09 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash =[]
+    ruby.each {|key, value|}
+    if new_hash.include ? (k)
+    languages_by_style[:ruby][:style] << style
+  else
+    key.each {|string, val|}
+    new_hash[:ruby]= {:type => val , :style => [style]}}
+  end
+  new_hash
 end
