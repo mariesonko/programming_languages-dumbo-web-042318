3754479d19e37932dc15a2e8bef465356044b25a
Marie Sonko <mariesonko62@gmail.com>
2018-04-22 02:25:11 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash =[]
+    ruby.each {|key, value|}
+    if new_hash.include ? (k)
+    languages[:ruby][:style] << style
+  else
+    key.each {|string, val|}
+    new_hash[:ruby]= {:type => val , :style => [style]}}
+  end
+  new_hash
 end
